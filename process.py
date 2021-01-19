faddf6daa105911ef798b8d7e768a556a3c71e1e
Rachel <rachelsuk97@gmail.com>
2021-01-19 23:28:40 +0000

    added comments explaining each line of code

--- process.py
+++ process.py
@@ -1,12 +1,16 @@
+# opens the txt file and saves content in variable "log_file"
 log_file = open("um-server-01.txt")
 
-
 def sales_reports(log_file):
+    # loops through each line of the txt file
     for line in log_file:
+        # removes any trailing characters
         line = line.rstrip()
+        # takes the first three characters of the line (day of week) and assigns them to variable 'day'
         day = line[0:3]
+        # if 'day' variable is "Tue", then print the contents of that line on the terminal
         if day == "Tue":
             print(line)
 
-
+# calls the 'sales_reports' function with argument 'log_file'
 sales_reports(log_file)
